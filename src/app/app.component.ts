15db427d109872a0ee189636f5f8f6abfd497970
SarahSantana <sarahsantanapereira@hotmail.com>
2020-09-28 19:33:48 -0300

    Adicionando mensagens fofinhas de boas vindas :)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gratitude-jar';
+
+  comprimentos = [
+    'e aí xuxu?',
+    'qual a boa bebê?',
+    'Bem-Vindo lindeza!',
+    'tutu bene?',
+    'E aí flor do deserto?',
+    'Oi migo! que saudades :)',
+    'Que bom que você está aqui!',
+    'Demorou hein xuxu? Volte mais vezes!'
+  ];
 }
