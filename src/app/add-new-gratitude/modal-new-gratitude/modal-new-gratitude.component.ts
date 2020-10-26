da5d8a4e8bb2469cd39f3a1a9fd639476d04c21d
SarahSantana <sarahsantanapereira@hotmail.com>
2020-10-26 20:36:48 -0300

    Adicionando novo componente que será um modal com formulário para cadastro de uma nova nota de gratidão

---
+++ src/app/add-new-gratitude/modal-new-gratitude/modal-new-gratitude.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-new-gratitude',
+  templateUrl: './modal-new-gratitude.component.html',
+  styleUrls: ['./modal-new-gratitude.component.scss']
+})
+export class ModalNewGratitudeComponent { }
